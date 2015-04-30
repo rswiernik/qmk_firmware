c5ee16242beea635ccb7b1cddce209605e5aece4
Reed Swiernik <rswiernik@csh.rit.edu>
2015-04-29 23:25:33 -0400

    Added mouse mode to keymap_reed.c

--- keyboard/planck/common_keymaps/keymap_reed.c
+++ keyboard/planck/common_keymaps/keymap_reed.c
@@ -20,38 +20,45 @@
  */
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = KEYMAP_GRID( /* Reed */
-  ESC,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC,
-  TAB,  A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT,
-  LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, FN5,
-  LCTL, CAPS, LALT, LGUI, FN2, FN7,  SPC,  FN1,   LEFT, DOWN, UP,  RGHT),
+  ESC,     Q,    W,    E,    R,    T,        Y,    U,    I,    O,    P, BSPC,
+  TAB,     A,    S,    D,    F,    G,        H,    J,    K,    L, SCLN, QUOT,
+  LSFT,    Z,    X,    C,    V,    B,        N,    M, COMM,  DOT, SLSH,  FN5,
+  LCTL,  FN7, LALT, LGUI,  FN2,  FN6,      SPC,  FN1, LEFT, DOWN,   UP, RGHT),
 
-[1] = KEYMAP_GRID( /* Reed EXTREME GAMING */
-  ESC,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC,
-  TAB,  A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT,
-  LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, FN5,
-  LCTL, 1,    2,    3,    4,   SPC,  FN2,  FN1,   LEFT, DOWN, UP,  RGHT),
+[1] = KEYMAP_GRID( /* Reed GAMING */
+  ESC,     Q,    W,    E,    R,    T,        Y,    U,    I,    O,    P, BSPC,
+  TAB,     A,    S,    D,    F,    G,        H,    J,    K,    L, SCLN, QUOT,
+  LSFT,    Z,    X,    C,    V,    B,        N,    M, COMM,  DOT, SLSH,  FN5,
+  LCTL,    1,    2,    3,    4,  SPC,      FN2,  FN1, LEFT, DOWN,   UP, RGHT),
 
 [2] = KEYMAP_GRID( /* Reed RAISE */
-  GRV,  1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    BSPC,
-  TRNS, FN3, FN4, PAUSE, TRNS, TRNS, TRNS, MINS, EQL,  LBRC, RBRC,  BSLS,
-  TRNS, F1,   F2,    F3,   F4,   F5,   F6,   F7,  F8,    F9,  F10,  TRNS,
-  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, FN1, MNXT, VOLD, VOLU, MPLY),
+   GRV,    1,    2,    3,    4,    5,        6,    7,    8,    9,    0, BSPC,
+  TRNS,  FN3,  FN4,PAUSE, TRNS, TRNS,     TRNS, MINS,  EQL, LBRC, RBRC, BSLS,
+  TRNS,   F1,   F2,   F3,   F4,   F5,       F6,   F7,   F8,   F9,  F10, TRNS,
+  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,     TRNS,  FN1, MNXT, VOLD, VOLU, MPLY),
 
 [3] = KEYMAP_GRID( /* Reed LOWER */
-  TRNS,  FN10, FN11, FN12, FN13, FN14, FN15, FN16, FN17, FN18, FN19, DEL,
-  TRNS,  TRNS, INS, HOME, PGUP, TRNS, TRNS, FN20, FN21, FN23, FN24, FN28,
-  TRNS,  TRNS, DEL,  END, PGDN,  F11,  F12,  F13,  TRNS, VOLD, VOLU, TRNS,
-  TRNS,  TRNS, TRNS, TRNS, FN2,  TRNS, TRNS, TRNS, MPRV, MUTE, MPLY, MNXT),
+  TRNS, FN10, FN11, FN12, FN13, FN14,     FN15, FN16, FN17, FN18, FN19,  DEL,
+  TRNS, TRNS,  INS, HOME, PGUP, TRNS,     TRNS, FN20, FN21, FN23, FN24, FN28,
+  TRNS, TRNS,  DEL,  END, PGDN,  F11,      F12,  F13, TRNS, VOLD, VOLU, TRNS,
+  TRNS, TRNS, TRNS, TRNS,  FN2, TRNS,     TRNS, TRNS, MPRV, MUTE, MPLY, MNXT),
+
+[4] = KEYMAP_GRID( /* Reed MOUSE */
+  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,     WH_L, WH_D, WH_U, WH_R, TRNS, TRNS,
+  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,     MS_L, MS_D, MS_U, MS_R, TRNS, TRNS,
+  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,     TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
+  TRNS, FN7, TRNS, TRNS, TRNS, BTN1,     BTN3, BTN2, TRNS, TRNS, TRNS, TRNS),
 };
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay - RAISE
-    [2] = ACTION_LAYER_MOMENTARY(3),  // to Fn overlay - LOWER
+    [1] = ACTION_LAYER_MOMENTARY(2),  // RAISE TO LAYER 2
+    [2] = ACTION_LAYER_MOMENTARY(3),  // LOWER TO LAYER 3
 
     [3] = ACTION_DEFAULT_LAYER_SET(0),
     [4] = ACTION_DEFAULT_LAYER_SET(1),
 	// Actions for the tap/hold modifiers listed above
     [5] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_ENT),
-    [7] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_BSPC),
+    [6] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_BSPC),
+    [7] = ACTION_LAYER_MOMENTARY(4),  // ACTIVATE MOUSE LAYER
 	
     [10] = ACTION_MODS_KEY(MOD_LSFT, KC_1),
     [11] = ACTION_MODS_KEY(MOD_LSFT, KC_2),
